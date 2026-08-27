813ce12f1a3eac0e38bfcc54a088c0e37d445ec8
esiolima <132793812+esiolima@users.noreply.github.com>
2026-08-27 10:41:20 -0300

    Update CardGenerator.tsx

--- client/src/pages/CardGenerator.tsx
+++ client/src/pages/CardGenerator.tsx
@@ -1718,12 +1718,12 @@ const journalCss = `
 
   .journal-category-bar{
     width:calc(100% - 72px);
-    min-height:116px;
+    min-height:216px;
     margin:38px auto 24px auto;
     background:#0f6bc8;
     color:white;
     display:grid;
-    grid-template-columns:100px minmax(0, 1fr) 100px;
+    grid-template-columns:200px minmax(0, 1fr) 100px;
     align-items:center;
     justify-content:center;
     gap:18px;
@@ -1747,7 +1747,7 @@ const journalCss = `
   }
 
   .journal-category-bar-image-slot{
-    width:100px;
+    width:200px;
     height:100px;
     border:0;
     border-radius:0;
